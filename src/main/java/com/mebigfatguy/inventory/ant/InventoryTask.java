73ba313bba83a7ffb47c949e5666cba69fb6d504
Dave Brosius <dbrosius@mebigfatguy.com>
2016-12-03 20:28:51 -0500

    initial commit

---
+++ src/main/java/com/mebigfatguy/inventory/ant/InventoryTask.java
@@ -0,0 +1,40 @@
+/*
+ * inventory - an ear/war inventory recorder
+ * Copyright 2016 MeBigFatGuy.com
+ * Copyright 2016 Dave Brosius
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and limitations
+ * under the License.
+ */
+package com.mebigfatguy.inventory.ant;
+
+import java.io.File;
+
+import org.apache.tools.ant.Task;
+
+public class InventoryTask extends Task {
+
+    private File archive;
+
+    public void setEar(File earFile) {
+        archive = earFile;
+    }
+
+    public void setWar(File warFile) {
+        archive = warFile;
+    }
+
+    @Override
+    public void execute() {
+
+    }
+}
