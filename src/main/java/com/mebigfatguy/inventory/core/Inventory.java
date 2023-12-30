acd64f78ea8b4bca4952c9af9dcc2e6a3d15a6eb
Dave Brosius <dbrosius@mebigfatguy.com>
2023-12-30 12:44:39 -0500

    try not to allow classes in same jar report on themselves

--- src/main/java/com/mebigfatguy/inventory/core/Inventory.java
+++ src/main/java/com/mebigfatguy/inventory/core/Inventory.java
@@ -34,170 +34,177 @@ import com.mebigfatguy.inventory.utils.NonClosingInputStream;
 
 public class Inventory implements AutoCloseable {
 
-    private File archive;
-    private Set<InventoryEventListener> listeners;
-    private InputStream stream;
-    private InputStream overrideStream;
-    private String activeJar;
+	private File archive;
+	private Set<InventoryEventListener> listeners;
+	private InputStream stream;
+	private InputStream overrideStream;
+	private String activeJar;
 
-    public Inventory(File archive) throws IOException {
-        this.archive = archive;
-        stream = new BufferedInputStream(Files.newInputStream(archive.toPath()));
-        listeners = new HashSet<>();
-    }
+	public Inventory(File archive) throws IOException {
+		this.archive = archive;
+		stream = new BufferedInputStream(Files.newInputStream(archive.toPath()));
+		listeners = new HashSet<>();
+	}
 
-    @Override
-    public void close() {
-        try {
-            stream.close();
-        } catch (IOException e) {
-        }
-    }
+	@Override
+	public void close() {
+		try {
+			stream.close();
+		} catch (IOException e) {
+		}
+	}
 
-    public void addInventoryEventListener(InventoryEventListener listener) {
-        listeners.add(listener);
-    }
+	public void addInventoryEventListener(InventoryEventListener listener) {
+		listeners.add(listener);
+	}
 
-    public void removeInventoryEventListener(InventoryEventListener listener) {
-        listeners.remove(listener);
-    }
+	public void removeInventoryEventListener(InventoryEventListener listener) {
+		listeners.remove(listener);
+	}
 
-    public InventoryReport takeInventory() throws InventoryException {
+	public InventoryReport takeInventory() throws InventoryException {
 
-        ArchiveScanner scanner;
+		ArchiveScanner scanner;
 
-        if (archive.getName().endsWith(".ear")) {
-            scanner = new EarScanner();
-        } else if (archive.getName().endsWith(".war")) {
-            scanner = new WarScanner();
-        } else {
-            throw new InventoryException("Unrecognized archive type: " + archive);
-        }
+		if (archive.getName().endsWith(".ear")) {
+			scanner = new EarScanner();
+		} else if (archive.getName().endsWith(".war")) {
+			scanner = new WarScanner();
+		} else {
+			throw new InventoryException("Unrecognized archive type: " + archive);
+		}
 
-        InventoryRecorder recorder = new InventoryRecorder();
-        try {
-            addInventoryEventListener(recorder);
-            scanner.scan(archive.getName(), this);
-            return recorder;
-        } catch (IOException e) {
-            getEventFirer().fireFailure(e.getMessage());
-            throw new InventoryException("Failed to read archive completely " + archive.getName(), e);
-        } finally {
-            removeInventoryEventListener(recorder);
-        }
-    }
+		InventoryRecorder recorder = new InventoryRecorder();
+		try {
+			addInventoryEventListener(recorder);
+			scanner.scan(archive.getName(), this);
+			return recorder;
+		} catch (IOException e) {
+			getEventFirer().fireFailure(e.getMessage());
+			throw new InventoryException("Failed to read archive completely " + archive.getName(), e);
+		} finally {
+			removeInventoryEventListener(recorder);
+		}
+	}
 
-    public InputStream getStream() {
-        if (overrideStream != null) {
-            return new NonClosingInputStream(overrideStream);
-        }
-        return new NonClosingInputStream(stream);
-    }
+	public InputStream getStream() {
+		if (overrideStream != null) {
+			return new NonClosingInputStream(overrideStream);
+		}
+		return new NonClosingInputStream(stream);
+	}
 
-    public void setStream(InputStream stream) {
-        overrideStream = stream;
-    }
+	public void setStream(InputStream stream) {
+		overrideStream = stream;
+	}
 
-    public void resetStream() {
-        overrideStream = null;
-    }
+	public void resetStream() {
+		overrideStream = null;
+	}
 
-    public InventoryEventFirer getEventFirer() {
-        return new InventoryEventFirer(listeners);
-    }
+	public InventoryEventFirer getEventFirer() {
+		return new InventoryEventFirer(listeners);
+	}
 
-    @Override
-    public String toString() {
-        return ToStringBuilder.reflectionToString(this, ToStringStyle.SHORT_PREFIX_STYLE);
-    }
+	@Override
+	public String toString() {
+		return ToStringBuilder.reflectionToString(this, ToStringStyle.SHORT_PREFIX_STYLE);
+	}
 
-    class InventoryRecorder implements InventoryEventListener, InventoryReport {
+	class InventoryRecorder implements InventoryEventListener, InventoryReport {
 
-        private Map<String, Set<String>> jarInventory = new HashMap<>();
-        private Map<String, Set<String>> packagesUsed = new HashMap<>();
-        private Map<String, Set<MethodDesc>> methodsUsed = new HashMap<>();
-        private Map<String, Set<MemberDesc>> membersUsed = new HashMap<>();
+		private Map<String, Set<String>> jarInventory = new HashMap<>();
+		private Map<String, Set<String>> packagesUsed = new HashMap<>();
+		private Map<String, Set<MethodDesc>> methodsUsed = new HashMap<>();
+		private Map<String, Set<MemberDesc>> membersUsed = new HashMap<>();
 
-        @Override
-        public void scanningWar(String warName, ScanStatus status) {
-        }
+		@Override
+		public void scanningWar(String warName, ScanStatus status) {
+		}
 
-        @Override
-        public void scanningJar(String jarName, ScanStatus status) {
-            if (status == ScanStatus.START) {
-                activeJar = jarName;
-            } else {
-                activeJar = null;
-            }
-        }
+		@Override
+		public void scanningJar(String jarName, ScanStatus status) {
+			if (status == ScanStatus.START) {
+				activeJar = jarName;
+			} else {
+				activeJar = null;
+			}
+		}
 
-        @Override
-        public void scanningFile(String fileName, ScanStatus status) {
-        }
+		@Override
+		public void scanningFile(String fileName, ScanStatus status) {
+		}
 
-        @Override
-        public void jarRecorded(String jarName) {
-        }
+		@Override
+		public void jarRecorded(String jarName) {
+		}
 
-        @Override
-        public void classRecorded(String clsName) {
+		@Override
+		public void classRecorded(String clsName) {
 
-        	Set<String> packages = jarInventory.get(activeJar);
-            if (packages == null) {
-                packages = new HashSet<>();
-                packagesUsed.put(activeJar, packages);
-            }
+			Set<String> packages = jarInventory.get(activeJar);
+			if (packages == null) {
+				packages = new HashSet<>();
+				packagesUsed.put(activeJar, packages);
+			}
 
-            int lastDotPos = clsName.lastIndexOf('.');
-            if (lastDotPos >= 0) {
-            	packages.add(clsName.substring(0, lastDotPos));
-            }
-        }
+			int lastDotPos = clsName.lastIndexOf('.');
+			String packageName = lastDotPos >= 0 ? clsName.substring(0, lastDotPos) : "";
+			packages.add(packageName);
+		}
 
-        @Override
-        public void classUsed(String packageName, String byFile) {
-            Set<String> packages = packagesUsed.get(byFile);
-            if (packages == null) {
-                packages = new HashSet<>();
-                packagesUsed.put(byFile, packages);
-            }
+		@Override
+		public void classUsed(String className, String byFile) {
+			if (!isInActiveJar(className)) {
+				Set<String> packages = packagesUsed.get(byFile);
+				if (packages == null) {
+					packages = new HashSet<>();
+					packagesUsed.put(byFile, packages);
+				}
 
-            packages.add(packageName);
-        }
-        
-        @Override
+				packages.add(className);
+			}
+		}
+
+		@Override
 		public void methodUsed(String className, String methodName, String signature, String byFile) {
-        	Set<MethodDesc> methods = methodsUsed.get(byFile);
-            if (methods == null) {
-            	methods = new HashSet<>();
-            	methodsUsed.put(byFile, methods);
-            }
+			if (!isInActiveJar(className)) {
+				Set<MethodDesc> methods = methodsUsed.get(byFile);
+				if (methods == null) {
+					methods = new HashSet<>();
+					methodsUsed.put(byFile, methods);
+				}
 
-            methods.add(new MethodDesc(className, methodName, signature));
+				methods.add(new MethodDesc(className, methodName, signature));
+			}
 		}
 
 		@Override
 		public void memberUsed(String className, String memberName, String byFile) {
-        	Set<MemberDesc> members = membersUsed.get(byFile);
-            if (members == null) {
-            	members = new HashSet<>();
-            	membersUsed.put(byFile, members);
-            }
+			if (!isInActiveJar(className)) {
+				Set<MemberDesc> members = membersUsed.get(byFile);
+				if (members == null) {
+					members = new HashSet<>();
+					membersUsed.put(byFile, members);
+				}
 
-            members.add(new MemberDesc(className, memberName));
+				members.add(new MemberDesc(className, memberName));
+			}
 		}
 
 		@Override
-        public void failure(String info) {
-        }
-        
-        public Map<String, Set<String>> getJarInventory() {
-        	return jarInventory;
-        }
-        
-        public Map<String, Set<String>> getPackagedUsed() {
-        	return packagesUsed;
-        }
+		public void failure(String info) {
+		}
+
+		@Override
+		public Map<String, Set<String>> getJarInventory() {
+			return jarInventory;
+		}
+
+		@Override
+		public Map<String, Set<String>> getPackagedUsed() {
+			return packagesUsed;
+		}
 
 		@Override
 		public Map<String, Set<MethodDesc>> getMethodsUsed() {
@@ -208,7 +215,13 @@ public class Inventory implements AutoCloseable {
 		public Map<String, Set<MemberDesc>> getMembersUsed() {
 			return membersUsed;
 		}
-        
-        
-    }
+
+		private boolean isInActiveJar(String className) {
+			int lastDotPos = className.lastIndexOf('.');
+			String packageName = lastDotPos >= 0 ? className.substring(0, lastDotPos) : "";
+			Set<String> packages = jarInventory.get(activeJar);
+			return packages != null && packages.contains(packageName);
+		}
+
+	}
 }
